89a41d14ac417ab1ce25838fce7bf9300cbbba36
mriwiki <71009631+Neonity2020@users.noreply.github.com>
2026-03-19 16:18:13 +0800

    feat: implement i18n and English version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  i18n: {
+    defaultLocale: 'zh',
+    locales: ['zh', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
